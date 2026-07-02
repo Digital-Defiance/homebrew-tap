a6aa9511d8012e0f120fa1338528da9b73c9c136
CIA Operations Officer Jennifer Pike <agent.jennifer.pike@gmail.com>
2026-07-01 17:15:51 -0700

    bsh-sdiagent 1.0.2

--- Casks/bsh-sdiagent.rb
+++ Casks/bsh-sdiagent.rb
@@ -1,8 +1,8 @@
 cask "bsh-sdiagent" do
-  version "1.0.1"
-  sha256 "76d0879c22f55bf0eefa5f865d28872e0f83831ca578a54f0c10868db4f7b536"
+  version "1.0.2"
+  sha256 "2b9a8581b558a01fd362614caed2321f750e536cfcce2c0c66e90ca0805174c0"
 
-  url "https://github.com/Digital-Defiance/bsh-sdi-agent/releases/download/v1.0.1/BSH-SDIAgent-1.0.1.zip"
+  url "https://github.com/Digital-Defiance/bsh-sdi-agent/releases/download/v1.0.2/BSH-SDIAgent-1.0.2.zip"
   name "BSH SDIAgent"
   desc "Secure Data Injection Agent for BrightShell"
   homepage "https://github.com/Digital-Defiance/bsh-sdi-agent"
