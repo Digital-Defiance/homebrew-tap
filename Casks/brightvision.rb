7e9ffe5da36728dde0160ac8cffe0e380731cdbb
CIA Operations Officer Jennifer Pike <agent.jennifer.pike@gmail.com>
2026-07-01 17:20:37 -0700

    brightvision 0.1.2-bright22

--- Casks/brightvision.rb
+++ Casks/brightvision.rb
@@ -1,6 +1,6 @@
 cask "brightvision" do
-  version "0.1.2-bright21"
-  sha256 "47b492e14ab6a8be9ba6d83033e00c588c116b583175822a1ac7364e0103945c"
+  version "0.1.2-bright22"
+  sha256 "0d8686e6cedf4e1ada0062342902c311530c8ecf275199fcbce87f357045397f"
 
   url "https://github.com/Digital-Defiance/BrightVision/releases/download/v#{version}/BrightVision_#{version}_universal.dmg"
   name "BrightVision"
