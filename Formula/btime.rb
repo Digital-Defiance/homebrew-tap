210dc09f3fda54634e01e179f1cbe66f7d158511
CIA Operations Officer Jennifer Pike <agent.jennifer.pike@gmail.com>
2026-07-01 17:13:16 -0700

    Initial commit

---
+++ Formula/btime.rb
@@ -0,0 +1,23 @@
+class Btime < Formula
+  desc "Time a command and display elapsed duration in BrightDate units"
+  homepage "https://github.com/Digital-Defiance/brightdate-rust"
+  url "https://github.com/Digital-Defiance/brightdate-rust/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "REPLACE_WITH_ACTUAL_SHA256_AFTER_RELEASE"
+  license "MIT"
+  head "https://github.com/Digital-Defiance/brightdate-rust.git", branch: "main"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "build", "--release", "--package", "btime"
+    bin.install "target/release/btime"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/btime --version")
+    # Time a trivially fast command; stderr should contain elapsed in millidays
+    output = shell_output("#{bin}/btime true 2>&1")
+    assert_match "real", output
+    assert_match "millidays", output
+  end
+end
