36134c8f17067feb0246764e9e17c79b866eba61
Agent Jennifer Pike <agent.jennifer.pike@gmail.com>
2026-07-01 17:16:02 -0700

    Update iputils

--- Formula/bsh-iputils.rb
+++ Formula/bsh-iputils.rb
@@ -1,14 +1,15 @@
-class BrightIputils < Formula
+class BSHIputils < Formula
   desc "Bright Spacetime/Date/Space-aware networking utilities"
   homepage "https://iputils.digitaldefiance.org"
-  url "https://github.com/Digital-Defiance/bright-iputils/archive/refs/tags/20260519.tar.gz"
-  sha256 "bbd2f5ab071b664981fc7f69559f1d447e8dc8ab0873a4b39fc519bd5d966aec"
+  url "https://github.com/Digital-Defiance/bright-iputils/archive/refs/tags/20260520.tar.gz"
+  sha256 "c08036c4379403527d25e5d6e880b8b075194b810698f3d006a96a89f5edd74c"
   license "BSD-3-Clause"
 
   depends_on "gettext"
   depends_on "meson"
   depends_on "ninja"
   depends_on "libidn2"
+  depends_on "bsh"
 
   def install
     system "meson", "setup", "builddir", *std_meson_args, "-DUSE_CAP=false"
