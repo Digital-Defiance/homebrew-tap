cd0671f186b685f15399718a25257cb5a524ddea
CIA Operations Officer Jennifer Pike <agent.jennifer.pike@gmail.com>
2026-07-01 17:17:36 -0700

    bright-vision 0.1.1-bright1

--- Casks/bright-vision.rb
+++ Casks/bright-vision.rb
@@ -1,13 +1,13 @@
 cask "bright-vision" do
-  version "0.1.0-bright7"
-  sha256 "f58d35e036ecb4b58a873d0b4048a278e1274c2edf38c86aaa3beeca60f7d132"
+  version "0.1.1-bright1"
+  sha256 "84bd22eaf42cf0d544a2c79cca0de1fc56d93c55b46dd2e7e25604c3fee2ddee"
 
-  url "https://github.com/Digital-Defiance/bright-vision/releases/download/v#{version}/Bright.Vision_#{version}_universal.dmg"
-  name "Bright Vision"
-  desc "A lightweight, cross-platform desktop application built with Tauri and React that provides a graphical interface to manage, configure, and interact with the Bright Vision Core AI coding assistant which is a headless version of Cecli with some improvements."
+  url "https://github.com/Digital-Defiance/bright-vision/releases/download/v#{version}/BrightVision_#{version}_universal.dmg"
+  name "BrightVision"
+  desc "A lightweight, cross-platform desktop application built with Tauri and React that provides a graphical interface to manage, configure, and interact with the BrightVision Core AI coding assistant which is a headless version of Cecli with some improvements."
   homepage "https://bright-vision.digitaldefiance.org"
 
   depends_on macos: :tahoe
 
-  app "Bright Vision.app"
+  app "BrightVision.app"
 end
