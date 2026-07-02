5fc63ce7a31e45362027a71b729c5633d27d1557
Agent Jennifer Pike <agent.jennifer.pike@gmail.com>
2026-07-01 17:16:16 -0700

    brightnexus 1.1.0

--- Casks/brightnexus.rb
+++ Casks/brightnexus.rb
@@ -1,8 +1,8 @@
 cask "brightnexus" do
-  version "1.0.9"
-  sha256 "3d8f917ea43098e98820a25d983aec42e3c17888880fa8ef33a286b49a08cb36"
+  version "1.1.0"
+  sha256 "3e714f1964fb2c2723fbbf90ecdce65d73b246579c6cf7b3ebd7318c65b8c717"
 
-  url "https://github.com/Digital-Defiance/BrightNexus/releases/download/v1.0.9/BrightNexus-1.0.9.zip"
+  url "https://github.com/Digital-Defiance/BrightNexus/releases/download/v1.1.0/BrightNexus-1.1.0.zip"
   name "BrightNexus"
   desc "Secure Enclave bridge and BrightLink credential agent"
   homepage "https://github.com/Digital-Defiance/BrightNexus"
