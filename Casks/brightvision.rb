d9c3db0ea3092b1093b968091f2c332f0fa9965a
Agent Jennifer Pike <agent.jennifer.pike@gmail.com>
2026-07-01 17:17:37 -0700

    update brightvision

--- Casks/brightvision.rb
+++ Casks/brightvision.rb
@@ -1,11 +1,11 @@
-cask "bright-vision" do
+cask "brightvision" do
   version "0.1.1-bright1"
   sha256 "84bd22eaf42cf0d544a2c79cca0de1fc56d93c55b46dd2e7e25604c3fee2ddee"
 
-  url "https://github.com/Digital-Defiance/bright-vision/releases/download/v#{version}/BrightVision_#{version}_universal.dmg"
+  url "https://github.com/Digital-Defiance/brightvision/releases/download/v#{version}/BrightVision_#{version}_universal.dmg"
   name "BrightVision"
   desc "A lightweight, cross-platform desktop application built with Tauri and React that provides a graphical interface to manage, configure, and interact with the BrightVision Core AI coding assistant which is a headless version of Cecli with some improvements."
-  homepage "https://bright-vision.digitaldefiance.org"
+  homepage "https://brightvision.digitaldefiance.org"
 
   depends_on macos: :tahoe
 
