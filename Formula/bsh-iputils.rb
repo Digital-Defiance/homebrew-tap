1bfdea2e3ec429a0fa2ec403b1d15fc36d1b27f8
CIA Operations Officer Jennifer Pike <agent.jennifer.pike@gmail.com>
2026-07-01 17:16:05 -0700

    update bsh

--- Formula/bsh-iputils.rb
+++ Formula/bsh-iputils.rb
@@ -5,14 +5,19 @@ class BshIputils < Formula
   sha256 "c08036c4379403527d25e5d6e880b8b075194b810698f3d006a96a89f5edd74c"
   license "BSD-3-Clause"
 
-  depends_on "gettext"
-  depends_on "meson"
-  depends_on "ninja"
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
   depends_on "libidn2"
   depends_on "bsh"
 
   def install
-    system "meson", "setup", "builddir", *std_meson_args, "-DUSE_CAP=false"
+    system "meson", "setup", "builddir", *std_meson_args,
+      "-DUSE_CAP=false",
+      "-DUSE_IDN=true",
+      "-DUSE_GETTEXT=false",
+      "-DBUILD_MANS=false",
+      "-DBUILD_HTML_MANS=false",
+      "-DSKIP_TESTS=true"
     system "ninja", "-C", "builddir"
     system "ninja", "-C", "builddir", "install"
   end
