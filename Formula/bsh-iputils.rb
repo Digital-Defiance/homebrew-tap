a9b5b2bc772ee00c23f8a9b059135f4eb269938e
Agent Jennifer Pike <agent.jennifer.pike@gmail.com>
2026-07-01 17:16:03 -0700

    fix iputils

--- Formula/bsh-iputils.rb
+++ Formula/bsh-iputils.rb
@@ -1,7 +1,7 @@
 class BSHIputils < Formula
   desc "Bright Spacetime/Date/Space-aware networking utilities"
   homepage "https://iputils.digitaldefiance.org"
-  url "https://github.com/Digital-Defiance/bright-iputils/archive/refs/tags/20260520.tar.gz"
+  url "https://github.com/Digital-Defiance/bsh-iputils/archive/refs/tags/20260520.tar.gz"
   sha256 "c08036c4379403527d25e5d6e880b8b075194b810698f3d006a96a89f5edd74c"
   license "BSD-3-Clause"
 
