13b5feea1c52072a01e47c5549f1c43522d9db31
Agent Jennifer Pike <agent.jennifer.pike@gmail.com>
2026-07-01 17:17:10 -0700

    update bright vision

--- Casks/bright-vision.rb
+++ Casks/bright-vision.rb
@@ -1,6 +1,6 @@
 cask "bright-vision" do
-  version "0.1.0-bright0"
-  sha256 "6d37553b49b1092dab5ebc559cd31badd8fb39cfe57642ac1b01f86dbaaf33e5"
+  version "0.1.0-bright1"
+  sha256 "1f9f4e379689f8b4f1b13ee244067cf691472763581c8db0b1be83a69918bf78"
 
   url "https://github.com/Digital-Defiance/bright-vision/releases/download/v#{version}/Bright.Vision_#{version}_universal.dmg"
   name "Bright Vision"
