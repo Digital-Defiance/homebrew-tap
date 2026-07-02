a0539c56210a8cb15d063bd80bf6187c782edfa8
Agent Jennifer Pike <agent.jennifer.pike@gmail.com>
2026-07-01 17:22:03 -0700

    brightvision 0.2.4-bright1

--- Casks/brightvision.rb
+++ Casks/brightvision.rb
@@ -1,6 +1,6 @@
 cask "brightvision" do
-  version "0.2.4"
-  sha256 "d24dcc23817fa9dd4ac124cfc5f27a669f556f0d48bad90645888df7951d98c9"
+  version "0.2.4-bright1"
+  sha256 "4308abe97c658bf25b5fb2ccbd39c15d0757a8f0e3d01d1d1338b6ecc8ab391f"
 
   url "https://github.com/Digital-Defiance/BrightVision/releases/download/v#{version}/BrightVision_#{version}_universal.dmg"
   name "BrightVision"
