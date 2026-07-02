6ff36889e51df75663267f090a4dca419ef40556
CIA Operations Officer Jennifer Pike <agent.jennifer.pike@gmail.com>
2026-07-01 17:27:01 -0700

    Use Warp_12 DMG filename for warp12 cask

--- Casks/warp12.rb
+++ Casks/warp12.rb
@@ -2,7 +2,7 @@ cask "warp12" do
   version "0.2.0"
   sha256 "a8467fdcfb2cf0cd2289a29c4443924de5b948530384347bc22a598fcae1968f"
 
-  url "https://github.com/Digital-Defiance/Warp12/releases/download/v#{version}/Warp%2012_#{version}_universal.dmg"
+  url "https://github.com/Digital-Defiance/Warp12/releases/download/v#{version}/Warp_12_#{version}_universal.dmg"
   name "Warp 12"
   desc "Star Trek-themed double-twelve Mexican Train dominoes (Tauri + React)"
   homepage "https://github.com/Digital-Defiance/Warp12"
