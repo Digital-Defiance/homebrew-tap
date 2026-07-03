d835cbf4f5d3680bd9791d08b6614f6fe3946f2c
Agent Jennifer Pike <agent.jennifer.pike@gmail.com>
2026-07-03 14:23:43 -0700

    warp12 0.4.3

--- Casks/warp12.rb
+++ Casks/warp12.rb
@@ -1,6 +1,6 @@
 cask "warp12" do
-  version "0.4.2"
-  sha256 "66a1a7052c25c8d9afb46917fdda430798683932c645d748f4ae17723f530aa9"
+  version "0.4.3"
+  sha256 "d0d0679be850f463e574680b11492a270fc5c3ba065265868dd8045c3bf9787a"
 
   url "https://github.com/Digital-Defiance/Warp12/releases/download/v#{version}/Warp_12_#{version}_universal.dmg"
   name "Warp 12"
